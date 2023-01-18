cc55e1207d8c1cbb25957a7fdc603917bc710d79
kaengland <kae83@cornell.edu>
2023-01-18 12:09:54 -0500

    Learned how to join

--- code/un_dataanalysis.R
+++ code/un_dataanalysis.R
@@ -47,6 +47,7 @@ gapminder_data%>%
   select(-continent) # REMOVES the column with the name
 
 # Pivot_wider-- pivots data value to make it a column heading
+# Usually this is just for sharing a table//readability..not so much for data analysis
 gapminder_data%>%
   select(country, continent, year, lifeExp)%>%
   pivot_wider(names_from = year, values_from = lifeExp)%>%
@@ -54,7 +55,32 @@ gapminder_data%>%
 
 
 
+#Working with messy data
+co2_emissions_dirty <- read_csv("data/co2-un-data.csv", skip = 2, #skips the first row, which seems to just be the title for the excel table, and the  column title row
+         col_names = c("region", "country", "year", "series", "value", "footnotes", "source")) # create new column names
+co2_emissions <- co2_emissions_dirty%>%
+  select(country, year, series, value)%>%
+  mutate(series = recode(series, "Emissions (thousand metric tons of carbon dioxide)" = "total_emissions", 
+                         "Emissions per capita (metric tons of carbon dioxide)"="per_capita_emissions"))%>%    #recode(column, if this, then replace with this)
+  pivot_wider(names_from=series, values_from=value)%>%
+  filter(year==2005)%>% # filter for year 2005
+  select(-year) # select to remove the year column
 
+# Bringing in 2007 population data
+gapminder_data_2007<-read_csv("data/gapminder_data.csv")%>%
+  filter(year==2007)%>%
+  select(country, pop, lifeExp, gdpPercap)
+
+# Let's join the tables based on country name
+# inner join discards data if there are not matching rows
+inner_join(co2_emissions, gapminder_data_2007, by = "country")
+
+#anti-join displays rows that are not shared
+#look at first data table, looking for rows that share values with the second data table, and KEEPS the FIRST data table values
+anti_join(co2_emissions, gapminder_data_2007)
+
+#full join-- combines tables and puts NA where data doesn't exist
+full_join(co2_emissions, gapminder_data_2007)
 
 
 
